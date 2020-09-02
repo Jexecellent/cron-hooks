e9c6d5e4e4078b6d363a400b338220c9a2271a59
Bearn <592374752@qq.com>
2020-09-02 17:54:30 +0800

    Update .fatherrc.js

--- .fatherrc.js
+++ .fatherrc.js
@@ -1,10 +1,4 @@
-/*
- * @Descripttion: 
- * @Author: Bean
- * @Date: 2020-09-01 09:36:59
- * @LastEditors: Bean
- * @LastEditTime: 2020-09-02 17:09:05
- */
+
 export default {
   entry: '/src/index',
   doc: {
